94395b6de9c0028b6851a86e4f3972ecd573387f
braudman <Braudman@gmail.com>
2019-04-19 20:02:12 -0400

    Update UserInfo.cpp

--- Functions-Coin flip/UserInfo.cpp
+++ Functions-Coin flip/UserInfo.cpp
@@ -1,7 +1,7 @@
  /****************************************************************************
  * AUTHOR	     : Stephen Braudo
- * STUDENT ID    : 1053220
- * Lab# 2		 : Coin Flip
+ * STUDENT ID        : 1053220
+ * Lab# 2	     : Coin Flip
  * CLASS	     : CS1B
  * SECTION	     : MW: 6:30pm
  * DUE DATE	     : 09/6/17
@@ -19,10 +19,10 @@
  *
  ******************************************************************************/
  void UserInfo( string &name,
-		 	    char   &gender,
-			    bool   &invalid)
+		char   &gender,
+		bool   &invalid)
  {
-	 const int PROMPT_COL = 27;
+	        const int PROMPT_COL = 27;
 
 	 	cout << left;
 	 	cout << setw(PROMPT_COL) << "What is your name?";
@@ -43,34 +43,34 @@
 	 			{
 
 	 			case 'M':
-	 					 invalid = 0;
-	 					 break;
+	 			  invalid = 0;
+	 			  break;
 	 			case 'F' :
-	 					 invalid = 0;
-	 					 break;
+	 			  invalid = 0;
+	 			  break;
 	 			default : invalid = 1;
 
 	 			}
 
 	 				if(invalid == false)
 	 				{
-	 					cout << "Try to get 3 heads in a row.";
+	 				    cout << "Try to get 3 heads in a row.";
 
 	 					if(toupper(gender)== 'M')
 	 					{
-	 						cout << " Good luck Mr. " << name << "!" << endl
-	 							 << endl;
+	 					    cout << " Good luck Mr. " << name << "!" << endl
+	 						 << endl;
 	 					}
 	 					else
 	 					{
-	 						cout << " Good luck Ms. " << name << "!" << endl
-	 							 << endl;
+	 					    cout << " Good luck Ms. " << name << "!" << endl
+	 					         << endl;
 	 					}
 	 				}
 	 				else
 	 				{
-	 					cout << "\n---Invalid Gender---\n"
-	 						 << "Please pick a gender!\n\n";
+	 				    cout << "\n---Invalid Gender---\n"
+	 					 << "Please pick a gender!\n\n";
 	 				}
 	 		}while(invalid);
 
